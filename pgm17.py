410b0b57cf29dfd066c57b0e43577d051dc8b39f
snambiaroff <144677752+snambiaroff@users.noreply.github.com>
2023-10-07 15:24:59 +0530

    Create pgm17.py

---
+++ pgm17.py
@@ -0,0 +1,11 @@
+def match_key_values(dict1, dict2):
+
+  matched_key_values = []
+  for key, value in dict1.items():
+    if key in dict2 and dict2[key] == value:
+      matched_key_values.append((key, value))
+  return matched_key_values
+
+
+dict1 = {'key1': 1, 'key2': 3, 'key3': 2}
+dict2 = {'key1': 1, 'key2': 2}
