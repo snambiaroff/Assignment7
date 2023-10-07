37dbd0d0a09434825faf2bba9a14adf3a0260bb3
snambiaroff <144677752+snambiaroff@users.noreply.github.com>
2023-10-07 15:21:22 +0530

    Create pgm13.py

---
+++ pgm13.py
@@ -0,0 +1,12 @@
+def remove_duplicates_from_dict(dict1):
+
+  unique_dict = {}
+  for key, value in dict1.items():
+    if key not in unique_dict:
+      unique_dict[key] = value
+  return unique_dict
+
+
+
+
+dict1 = {1: 10, 2: 20, 3: 30, 1: 10, 2: 20}
